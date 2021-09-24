99e529c16d79f145f2cf2f7cb37f6c6f1580f550
ansanmathew <73578269+penguinlips@users.noreply.github.com>
2021-09-25 03:47:00 +0530

    Add files via upload

---
+++ assets/js/wht.js
@@ -0,0 +1,13 @@
+function printDiv() {
+    var divName = document.getElementById("name").value;
+    var divPlace = document.getElementById("place").value;
+    var divSubject = document.getElementById("subject").value;
+    var divMessage = document.getElementById("message").value;
+    if (divName == "" || divPlace == "" || divMessage == "" ) {
+        window.alert("Name, Place and Message are mandatory!");
+    } else {
+    window.open("https://wa.me/+919747994523/?text=%09"+divSubject+"%0A%0A"+divMessage+"%0A%0AName%20:%20"+divName+"%0APlace%20:%20"+divPlace);
+    
+    }
+}
+
